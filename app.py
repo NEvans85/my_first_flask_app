7086c1d31d250e68c062672648b7313df8c0daa4
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-02 12:45:12 -0800

    add tutorial address

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# following tutorial at https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
